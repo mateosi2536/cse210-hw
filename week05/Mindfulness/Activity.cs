8fed0866665d77b7c7cb80be4b4cfab96ff8b54a
mateosi2536 <mateoszambranos@gmail.com>
2025-11-29 07:31:55 -0500

    Activityes

---
+++ week05/Mindfulness/Activity.cs
@@ -0,0 +1,92 @@
+using System;
+using System.Collections.Generic;
+using System.Threading;
+
+public class Activity
+{
+    protected string _name;
+    protected string _description;
+    protected int _duration;
+
+    public Activity()
+    {
+    }
+
+    public void DisplayStartingMessage()
+    {
+        Console.Clear();
+        Console.WriteLine($"Welcome to the {_name}.");
+        Console.WriteLine();
+        Console.WriteLine(_description);
+        Console.WriteLine();
+        Console.Write("How long, in seconds, would you like for your session? ");
+        while (true)
+            try
+            {
+                _duration = int.Parse(Console.ReadLine());
+                break;
+            }
+            catch (FormatException)
+            {
+                Console.WriteLine("Invalid input. Please enter a valid number for the duration.");
+                Console.Write("How long, in seconds, would you like for your session? ");
+            }
+
+        Console.Clear();
+        Console.WriteLine("Get ready...");
+        ShowSpinner(5);
+    }
+
+    public void DisplayEndingMessage()
+    {
+        Console.WriteLine();
+        Console.WriteLine("Well done!!");
+        ShowSpinner(5);
+        Console.WriteLine();
+        Console.WriteLine($"You have completed another {_duration} seconds of the {_name}.");
+        ShowSpinner(5);
+    }
+
+    public void ShowSpinner(int seconds)
+    {
+        List<string> animationStrings = new List<string>();
+        animationStrings.Add("|");
+        animationStrings.Add("/");
+        animationStrings.Add("-");
+        animationStrings.Add("\\");
+        animationStrings.Add("|");
+        animationStrings.Add("/");
+        animationStrings.Add("-");
+        animationStrings.Add("\\");
+
+        DateTime startTime = DateTime.Now;
+        DateTime endTime = startTime.AddSeconds(seconds);
+
+        int i = 0;
+
+        while (DateTime.Now < endTime)
+        {
+            string s = animationStrings[i];
+            Console.Write(s);
+            Thread.Sleep(1000);
+            Console.Write("\b \b");
+
+            i++;
+
+            if (i >= animationStrings.Count)
+            {
+                i = 0;
+            }
+        }
+    }
+
+    public void ShowCountDown(int seconds)
+    {
+        for (int i = seconds; i > 0; i--)
+        {
+            Console.Write(i);
+            Thread.Sleep(1000);
+            Console.Write("\b \b");
+        }
+    }
+}
--- week05/Mindfulness/Program.cs
+++ week05/Mindfulness/Program.cs
@@ -1,9 +1,62 @@
 using System;
+using System.Collections.Generic;
 
 class Program
 {
+    // Creativity: Keep a log of how many times activities were performed.
+    static Dictionary<string, int> activityLog = new Dictionary<string, int>
+    {
+        { "Breathing Activity", 0 },
+        { "Reflection Activity", 0 },
+        { "Listing Activity", 0 }
+    };
+
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Mindfulness Project.");
+        while (true)
+        {
+            Console.Clear();
+            Console.WriteLine("Menu Options:");
+            Console.WriteLine("  1. Start breathing activity");
+            Console.WriteLine("  2. Start reflection activity");
+            Console.WriteLine("  3. Start listing activity");
+            Console.WriteLine("  4. Quit");
+            Console.Write("Select a choice from the menu: ");
+            string choice = Console.ReadLine();
+
+            if (choice == "1")
+            {
+                BreathingActivity breathingActivity = new BreathingActivity();
+                breathingActivity.Run();
+                activityLog["Breathing Activity"]++;
+            }
+            else if (choice == "2")
+            {
+                ReflectionActivity reflectionActivity = new ReflectionActivity();
+                reflectionActivity.Run();
+                activityLog["Reflection Activity"]++;
+            }
+            else if (choice == "3")
+            {
+                ListingActivity listingActivity = new ListingActivity();
+                listingActivity.Run();
+                activityLog["Listing Activity"]++;
+            }
+            else if (choice == "4")
+            {
+                DisplayLog();
+                break;
+            }
+        }
+    }
+
+    static void DisplayLog()
+    {
+        Console.WriteLine();
+        Console.WriteLine("Activity Log:");
+        foreach (var entry in activityLog)
+        {
+            Console.WriteLine($"{entry.Key}: {entry.Value} times");
+        }
     }
 }
